19c455da2c5130e544e56cc5bb304d0918b529fd
jk4g2 <jk4g2@mail.missouri.edu>
2020-03-10 16:28:53 -0500

    added error handling by if and else

--- api.py
+++ api.py
@@ -7,11 +7,11 @@ api_key="3de2caa5b3b3dc2a07650bbf658738c7"
 #put artist name and track title.
 #if they have space between their name or trackTitle, then put & instaed of space
 #for example justin%20bieber
-artist = "bts"
-trackTitle = "on"
+artist_input = "post%20malone"
+trackTitle_input = "circles"
 
 #gettrack Info
-url = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key="+api_key+"&artist="+ artist +"&track="+trackTitle+"&format=json"
+url = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key="+api_key+"&artist="+ artist_input +"&track="+trackTitle_input+"&format=json"
 response = urllib.request.urlopen(url).read()
 json_obj = str(response,'utf-8')
 data = json.loads(json_obj)
@@ -21,22 +21,29 @@ if 'mbid' in data['track']:
 else:
     track_ID = "null"
 
+album_title = data['track']['album']['title']
+if 'mbid' in data['track']['album']:
+    album_ID = data['track']['album']['mbid']
+else:
+    album_ID = "null"
+
 #getArtist Info
-url_for_artist = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist="+artist+"&api_key="+api_key+"&format=json"
+url_for_artist = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist="+artist_input+"&api_key="+api_key+"&format=json"
 response_for_artist = urllib.request.urlopen(url_for_artist).read()
 json_obj_for_artist = str(response_for_artist,'utf-8')
 data_for_artist = json.loads(json_obj_for_artist)
 artist_name = data_for_artist['artist']['name']
 if 'mbid' in data_for_artist['artist']:
-    artistID = data_for_artist['artist']['mbid']
+    artist_ID = data_for_artist['artist']['mbid']
 else:
-    artistID = "null"
+    artist_ID = "null"
 
-
+#print out the data that I grabbed
 print('trackTitle: ' + track_title)
 print('trackID: ' + track_ID)
 print('artist: ' + artist_name)
-print('artistID: ' + artistID)
-#print("albumTitle : " + album_title)
+print('artistID: ' + artist_ID)
+print('album: ' +album_title)
+print('album: ' + album_ID)
 
 #going to put those into the database

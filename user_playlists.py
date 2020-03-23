8ea662d2af9091528ba76a40bd1ab6c0320d7a77
Tommy <tregf7@mail.missouri.edu>
2020-03-23 18:58:12 -0500

    Using spotipy to read user data

---
+++ user_playlists.py
@@ -0,0 +1,42 @@
+# shows a user's playlists
+
+import sys
+import spotipy
+import spotipy.util as util
+
+
+def show_tracks(tracks):
+    for i, item in enumerate(tracks['items']):
+        track = item['track']
+        print("   %d %32.32s %s" % (i, track['artists'][0]['name'],
+            track['name']))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
+    else:
+        print("Whoops, need your username!")
+        print("usage: python user_playlists.py [username]")
+        sys.exit()
+    
+    # below line provides access token using authorization code flow, but hopefully we can repurpose (with correct scope) from iOS end
+    token = util.prompt_for_user_token(username)
+    
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        playlists = sp.user_playlists(username)
+        for playlist in playlists['items']:
+            if playlist['owner']['id'] == username:
+                print()
+                print(playlist['name'])
+                print ('  total tracks', playlist['tracks']['total'])
+                results = sp.playlist(playlist['id'],
+                    fields="tracks,next")
+                tracks = results['tracks']
+                show_tracks(tracks)
+                while tracks['next']:
+                    tracks = sp.next(tracks)
+                    show_tracks(tracks)
+    else:
+        print("Can't get token for", username)

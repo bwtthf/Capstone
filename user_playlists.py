b220ee1dcac67c97e0455fe32ba86f1e3d31d8ab
jk4g2 <jk4g2@mail.missouri.edu>
2020-04-04 22:09:06 -0500

    I grabbed the music data by using Spotify Api

--- user_playlists.py
+++ user_playlists.py
@@ -3,14 +3,34 @@
 import sys
 import spotipy
 import spotipy.util as util
+import json
+import db_conn
+from db_conn import cnx
 
-
-def show_tracks(tracks):
+#vr7wzjpfxp2bdgsbd63ewwaub
+def show_tracks(tracks,sp1):
     for i, item in enumerate(tracks['items']):
         track = item['track']
-        print("   %d %32.32s %s" % (i, track['artists'][0]['name'],
-            track['name']))
-
+        artist = track['artists'][0]['name']
+        trackTitle = track['name']
+        searchResult = sp1.search(q='track:' + trackTitle, type='track', limit=50)
+        print()
+        for i in range(0,len(searchResult['tracks']['items'])):
+            for y in range(0,len(searchResult['tracks']['items'][i]['album']['artists'])):
+                if searchResult['tracks']['items'][i]['album']['artists'][y]['name'] == artist:
+                    #artist mbid
+                    print(searchResult['tracks']['items'][i]['album']['artists'][y]['id'])
+                    #artist name
+                    print(searchResult['tracks']['items'][i]['album']['artists'][y]['name'])
+                    #album name
+                    print(searchResult['tracks']['items'][i]['album']['name'])
+                    #album uri need to use greb later
+                    print(searchResult['tracks']['items'][i]['album']['uri'])
+                    #track name
+                    print(searchResult['tracks']['items'][i]['name'])
+                    #track id
+                    print(searchResult['tracks']['items'][i]['id'])
+                    print()
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -31,12 +51,11 @@ if __name__ == '__main__':
                 print()
                 print(playlist['name'])
                 print ('  total tracks', playlist['tracks']['total'])
-                results = sp.playlist(playlist['id'],
-                    fields="tracks,next")
+                results = sp.playlist(playlist['id'], fields="tracks,next")
                 tracks = results['tracks']
-                show_tracks(tracks)
+                show_tracks(tracks,sp)
                 while tracks['next']:
                     tracks = sp.next(tracks)
-                    show_tracks(tracks)
+                    show_tracks(tracks,sp)
     else:
         print("Can't get token for", username)
--- user_tracks.py
+++ user_tracks.py
@@ -19,17 +19,60 @@ if token:
     if not results:
         print("no saved track")
     else:
-        for item in results['items']:
+         for item in results['items']:
             track = item['track']
-            #print(track['name'] + ' - ' + track['artists'][0]['name'])
-            title = track['name']
             artist = track['artists'][0]['name']
-            print(title)
-            print(artist)
-            searchResult = sp1.search(q='track:'+title, type='track', limit=50)
-            
-            print(json.dumps(searchResult, sort_keys=True, indent=4))
-            with open("text1.txt", "a") as myfile:
-                myfile.write(json.dumps(searchResult, sort_keys=True, indent=4))
+            trackTitle = track['name']
+
+            searchResult = sp1.search(q='track:'+trackTitle, type='track', limit=50)
+            print()
+            for i in range(0,len(searchResult['tracks']['items'])):
+                for y in range(0,len(searchResult['tracks']['items'][i]['album']['artists'])):
+                    if searchResult['tracks']['items'][i]['album']['artists'][y]['name'] == artist:
+                        #artist mbid
+                        artist_ID = searchResult['tracks']['items'][i]['album']['artists'][y]['id']
+                        print(searchResult['tracks']['items'][i]['album']['artists'][y]['id'])
+
+                        #artist name
+                        artist_name = searchResult['tracks']['items'][i]['album']['artists'][y]['name']
+                        print(searchResult['tracks']['items'][i]['album']['artists'][y]['name'])
+
+                        #album name
+                        album_title = searchResult['tracks']['items'][i]['album']['name']
+                        print(searchResult['tracks']['items'][i]['album']['name'])
+
+                        #album uri need to use greb later
+                        print(searchResult['tracks']['items'][i]['album']['uri'])
+
+                        #track name
+                        track_title = searchResult['tracks']['items'][i]['name']
+                        print(searchResult['tracks']['items'][i]['name'])
+
+                        #track id
+                        trackTitle = searchResult['tracks']['items'][i]['id']
+                        print(searchResult['tracks']['items'][i]['id'])
+
+                        #print(searchResult['tracks']['items'][i]['album']['artists'][y]['name'])
+            #    if searchResult['tracks']['items'][0]['album']['name'] == artist:
+            #        print(searchResult['tracks']['items'][0]['album']['name'])
+
+            #artistmbid
+            #print(searchResult['tracks']['items'][0]['album']['artists'][0]['id'])
+            #album name
+            #print(searchResult['tracks']['items'][0]['album']['name'])
+            #album mbid
+            #print(searchResult['tracks']['items'][0]['album']['id'])
+            #track mbid
+            #print(searchResult['tracks']['items'][0]['id'])
+
+            #if 'name' in searchResult['tracks']['items']['albums']:
+            #    if searchResult['tracks']['items']['albums']['name'] == artist:
+            #        print(artist)
+
+            #with open("text1.txt", "a") as myfile:
+            #    myfile.write(json.dumps(searchResult, sort_keys=True, indent=4))
 else:
     print("Can't get token for", username)
+
+print("success")
+cnx.close()

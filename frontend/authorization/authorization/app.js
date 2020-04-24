4d204af4665932de7555b798c9e775afac144965
tertl700 <tommy.ertl@gmail.com>
2020-04-24 16:42:50 +0000

    Changed child processes to run python3

--- frontend/authorization/authorization/app.js
+++ frontend/authorization/authorization/app.js
@@ -159,12 +159,12 @@ app.get('/callback', function(req, res) {
         const logOutput = (name) => (data) => console.log(`[${name}] ${data.toString()}`)
         //console.log(path)
         setTimeout(() => {
-          const tracks = spawn('python', [path.join(__dirname, '../../../src/top_tracks.py')]);
+          const tracks = spawn('python3', [path.join(__dirname, '../../../src/top_tracks.py')]);
           tracks.stdout.on('data', logOutput('stdout'));
           tracks.stderr.on('data', logOutput('stderr'));
         }, 200);
         setTimeout(() => {
-          const matches = spawn('python', [path.join(__dirname, '../../../src/get_matches.py')]);
+          const matches = spawn('python3', [path.join(__dirname, '../../../src/get_matches.py')]);
           matches.stdout.on('data', logOutput('stdout'));
           matches.stderr.on('data', logOutput('stderr'));
         }, 3000);

8aa43b6aa8e81481843b7620cd87526f05ee1954
tertl700 <tommy.ertl@gmail.com>
2020-04-26 12:44:52 -0500

    Fixed two way matching and messy JSON strings

--- src/get_matches.py
+++ src/get_matches.py
@@ -9,8 +9,9 @@ userIDs = []
 cursor = cnx.cursor(buffered=True)
 sql = 'UPDATE user SET updated = IF(updated = "2", "1", updated)'
 cursor.execute(sql)
-
+cursor.close()
 while True:
+    cursor = cnx.cursor(buffered=True)
     sql = 'SELECT userID FROM user WHERE updated = "1"'
     cursor.execute(sql)
     users = 0
@@ -20,9 +21,9 @@ while True:
         genres = {}
         get_genres = {}
         genres['userID'] = userID
-        print(userID)
+        
         sql = 'SELECT genres FROM user WHERE userID = %s'
-        cursor = cnx.cursor(dictionary=True)
+        cursor = cnx.cursor(buffered=True, dictionary=True)
         cursor.execute(sql, (userID,))
         for g in cursor:
             get_genres = g['genres']
@@ -32,7 +33,6 @@ while True:
     if users <= 1:
         print('Requires at least two users to attempt matching')
         break
-    #print(dicts)
 
     for i in range(users - 1):
         matching_artists = 0
@@ -56,28 +56,56 @@ while True:
                 unique_genres.append(word)
         num_matches = (matching_artists, len(unique_genres), userIDs[i + 1])
         matches.append(num_matches)
-    print(matches)
+    
+    # store matches in list
+    for tup in matches:
+        if tup[0] > 0 or tup[1] > 5:
+            user_matches.append(tup[2])
+
+
+    # get current matches from db
+    cursor = cnx.cursor(buffered=True)
+    sql = 'SELECT matches FROM user WHERE userID = %s'
+    cursor.execute(sql, (userIDs[0],))
+
+    for (match,) in cursor:
+        if match == '{}':
+            init_match = {userIDs[0] : user_matches}
+            sql = 'UPDATE user SET matches = %s WHERE userID = %s'
+            cursor.execute(sql, (json.dumps(init_match), userIDs[0]))
+            cnx.commit()
+        else:
+            match = json.loads(match)
+            for user in user_matches:
+                sql = 'SELECT matches FROM user WHERE userID = %s'
+                cursor.execute(sql, (user,))
+                # update matched users' matches with original user
+                for (users_match,) in cursor:
+                    users_match = json.loads(users_match)
+                    if userIDs[0] not in users_match[user]:
+                        users_match[user].append(userIDs[0])
+                        sql = 'UPDATE user SET matches = %s WHERE userID = %s'
+                        cursor.execute(sql, (json.dumps(users_match), user))
+                        cnx.commit()
+
+                # update current matches with newer matches
+                if user not in match[userIDs[0]]:
+                    match[userIDs[0]].append(user)
+                    sql = 'UPDATE user SET matches = %s WHERE userID = %s'
+                    cursor.execute(sql, (json.dumps(match), userIDs[0]))
+                    cnx.commit()
+            
     
     #update MySQL to reflect already matched users
     sql = 'UPDATE user SET updated = "2" WHERE userID = %s'
     cursor.execute(sql, (userIDs[0],))
     cnx.commit()
 
-    # store matches in list
-    for tup in matches:
-        if tup[0] > 0 or tup[1] > 5:
-            user_matches.append(tup[2])
-    # update mysql db with list of matches
-    sql = 'UPDATE user SET matches = COALESCE(JSON_MERGE_PRESERVE(%s, JSON_SET(matches, "$", %s)), JSON_ARRAY("[]")) WHERE userID = %s'
-    cursor.execute(sql, (json.dumps(userIDs[0]), json.dumps(user_matches), userIDs[0]))
-    cnx.commit()
-    for user in user_matches:
-        sql = 'UPDATE user SET matches = COALESCE(JSON_MERGE_PRESERVE(%s, JSON_SET(matches, "$", %s)), JSON_ARRAY("[]")) WHERE userID = %s'
-        cursor.execute(sql, (json.dumps(user), json.dumps(userIDs[0]), user))
-        cnx.commit()
     userIDs.clear()
     matches.clear()
+    user_matches.clear()
     dicts.clear()
+    cursor.close()
     
 
     

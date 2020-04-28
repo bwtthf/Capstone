612c294e26714319927cfe85c677f0664bf07d16
brent thayer <bwtthf@mail.missouri.edu>
2020-04-27 19:50:42 -0500

    smoother navigation

--- frontend/authorization/authorization/app.js
+++ frontend/authorization/authorization/app.js
@@ -107,7 +107,7 @@ app.get('/callback', function(req, res) {
         });
         
         // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:8100/login/'); //+
+        res.redirect('http://localhost:8100/navigation/'); //+
         //  querystring.stringify({
         //    access_token: access_token,
         //    refresh_token: refresh_token
